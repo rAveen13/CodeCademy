6f3c93cc5ed325b0f8b85bba34216b26ff051067
rAveen13 <75273743+rAveen13@users.noreply.github.com>
2023-11-29 21:28:39 +0100

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "  A  " << "   " << "M   M" << endl;
+cout << " A A " << "   " << "MM MM" << endl;
+cout << "A   A" << "   " << "MM MM" << endl;
+cout << "AAAAA" << "   " << "M M M" << endl;
+cout << "A   A" << "   " << "M   M" << endl;
+cout << "A   A" << "   " << "M   M" << endl;
+cout << "A   A" << "   " << "M   M" << endl;
+
+
+  return 0;
+}
