739aaa1305b7bf6c8d61b786345bf20752aa5b7f
rAveen13 <75273743+rAveen13@users.noreply.github.com>
2023-11-29 21:27:52 +0100

    Create dog_years.cpp

---
+++ dog_years.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int age = 8;
+int early_years = 21;
+int later_years = (age - 2) * 4;
+int human_years = early_years + later_years;
+
+
+int main() {
+
+  cout << "My name is Quintel! Ruff ruff, I am " << human_years << " years old in human years.";
+
+  return 0;
+
+}
